e83cf820790769e6d062362f63375d0b2f0487ed
mnlg_ <ivanrise@gmail.com>
2014-02-24 11:50:09 +0100

    Change port number and install nginx Change port number and install nginx in virtual machine

--- provision.sh
+++ provision.sh
@@ -12,11 +12,15 @@ if [ ! -f /vagrant/provision.lock ]; then
   apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
   echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
 
+  # add nginx repository
+  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C300EE8C
+  echo "deb http://ppa.launchpad.net/nginx/stable/ubuntu lucid main" | tee /etc/apt/sources.list.d/nginx-stable-lucid.list
+
   # update ubuntu pacakges
   apt-get -y update
 
   # install packages
-  apt-get -y install python-software-properties python g++ make mongodb-10gen nodejs git
+  apt-get -y install python-software-properties python g++ make mongodb-10gen nodejs git nginx
 
   # create the provisioning lock file
   touch /vagrant/provision.lock
