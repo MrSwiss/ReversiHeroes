e8b522cee79d891205077adf78c50d87f18efa7a
mnlg_ <ivanrise@gmail.com>
2014-06-13 10:20:04 +0200

    media queries improvements

--- public/js/common.js
+++ public/js/common.js
@@ -5,3 +5,17 @@ $('#mobile-menu').on('click', function(e) {
   e.preventDefault();
   $('#navigation').toggle();
 });
+
+$('#signin-btn').on('click', function(e) {
+  e.preventDefault();
+  $('#home-btns').hide();
+  $('#signin-block').show();
+  $('#logo-img').hide();
+});
+
+$('#cancel-btn').on('click', function(e) {
+  e.preventDefault();
+  $('#home-btns').show();
+  $('#signin-block').hide();
+  $('#logo-img').show();
+});
